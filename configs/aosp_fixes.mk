spezi77/abc_vendor_nexus

Fix AOSP gesture typing

configs/aosp_fixes.mk
@@ -28,10 +28,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Proprietary latinime libs needed for Keyboard swyping
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
-    vendor/nexus/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+    vendor/nexus/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/nexus/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 else
 PRODUCT_COPY_FILES += \
-    vendor/nexus/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+    vendor/nexus/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/nexus/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 # Camera Effects for devices without a vendor partition

GhoshRom/vendor_ghoshrom

configs/aosp_fixes.mk
@@ -27,4 +27,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Proprietary latinime libs needed for Keyboard swyping
 
 PRODUCT_COPY_FILES += \
-    vendor/ghost/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+    vendor/ghost/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/ghost/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+  